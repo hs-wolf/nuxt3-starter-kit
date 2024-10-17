290d204ee2c7adec61f04f8c6360fb4d2765149b
José Israel <hswolf@proton.me>
2024-10-17 17:21:51 -0300

    feat(package): Installed the nuxt i18n module, configured it and created two lang files, for english and portuguese.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,12 +11,13 @@ export default defineNuxtConfig({
       meta: [
         { name: 'description', content: 'Nuxt3 Starter Kit' },
       ],
-      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: 'favicon.ico' }],
     },
   },
   modules: [
     '@nuxt/eslint',
     '@nuxtjs/tailwindcss',
+    '@nuxtjs/i18n',
   ],
   eslint: {
     config: {
@@ -25,5 +26,29 @@ export default defineNuxtConfig({
   },
   tailwindcss: {
     viewer: false,
+  },
+  i18n: {
+    locales: [
+      {
+        name: 'English',
+        code: 'en',
+        language: 'en-US',
+        file: 'en.yaml',
+      },
+      {
+        name: 'Português',
+        code: 'pt',
+        language: 'pt-BR',
+        file: 'pt.yaml',
+      },
+    ],
+    defaultLocale: 'en',
+    lazy: true,
+    langDir: 'lang',
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: 'i18n_redirected',
+      redirectOn: 'root',
+    },
   },
 })
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-
+const { locale: currentLocale, locales } = useI18n()
+const switchLocalePath = useSwitchLocalePath()
 </script>
 
 <template>
@@ -11,6 +12,20 @@
       <li>Nuxt Devtools</li>
       <li>Linting with Eslint and Antfu Eslint Config</li>
       <li>CSS framework with TailwindCSS and SASS</li>
+      <li>
+        <div class="flex items-center gap-2">
+          <span>Localization with i18n</span>
+          <span>-</span>
+          <NuxtLink
+            v-for="locale in locales"
+            :key="locale.code"
+            :to="switchLocalePath(locale.code)"
+            :class="currentLocale === locale.code ? 'text-green-600' : 'text-white'"
+          >
+            {{ $t(`languages.${locale.code}`) }}
+          </NuxtLink>
+        </div>
+      </li>
     </ul>
   </div>
 </template>
